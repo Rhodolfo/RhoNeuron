3723318d8b039adf2a712b84643dd23d8e8d166f
Rhodolfo <rhonavarr@gmail.com>
2017-04-24 11:13:36 -0500

    Tested XOR gate, changed data structures

--- src/main/scala/neuron.scala
+++ src/main/scala/neuron.scala
@@ -4,9 +4,10 @@ object neurons {
 
   // Custom types
   type Real = Double
-  type Signals = List[Double]
-  def Signals(xs: Real*) = List(xs: _*)
-  type Layer = List[Neuron]
+  type Signals = Array[Real]
+  def Signals(xs: Real*) = Array(xs: _*)
+  type Layer = Array[Neuron]
+  def Layer(xs: Neuron*) = Array(xs: _*)
 
   // Basic neuron classes
   abstract class Neuron(w: Signals, b: Real) {
@@ -16,6 +17,10 @@ object neurons {
     }
     def process(z: Real): Real
     def apply(x: Signals): Real = process(argument(x))
+    override def toString(): String = {
+      val ws = w.foldLeft[String]("")((a,b) => if (a.isEmpty) b.toString else a+","+b.toString)
+      "Neuron[weights=("+ws+"),bias="+b+"]"
+    }
   }
 
   class Perceptron(w:Signals, b: Real) extends Neuron(w,b) {
@@ -27,15 +32,23 @@ object neurons {
   }
 
   // Neural network
-  class Network(network: List[Layer]) {
+  class Network(layers: List[Layer]) {
+
+    override def toString(): String = {
+      def  concat(s:String)(a:String,b:String): String = if (a.isEmpty) b else a+s+b
+      def layer2string(layer: Layer): String = layer.foldLeft[String]("")((a,b)=>concat(",")(a,b.toString))
+      val ls = layers.foldLeft[String]("")((a,b) => concat("\n")(a,layer2string(b)))
+      "Network of "+layers.size+ " layers:\n"+ls
+    }
 
     def backSweep(x: Signals): List[Signals] = {
       def build(net: List[Layer], acc: List[Signals]): List[Signals] = {
         if (net.isEmpty) acc
         else build(net.tail, net.head.map(neuron => neuron apply acc.head) :: acc)
       }
-      build(network,List(x))
+      build(layers,List(x))
     }
+
 
   }
 
--- src/test/scala/neural.scala
+++ src/test/scala/neural.scala
@@ -1,6 +1,6 @@
 import org.scalatest.FunSuite
 
-class DiskTesting extends FunSuite {
+class NeuralTesting extends FunSuite {
 
   import com.rho.neurons._
 
@@ -17,6 +17,30 @@ class DiskTesting extends FunSuite {
     assert(0.5===neuron(Signals((-2.0))))
     assert(0.5<neuron(Signals((2.0))))
     assert(0.5>neuron(Signals((-5.0))))
+  }
+
+  test("XOR Gate, Perceptron Network") {
+    val top = new Perceptron(Signals(0.6,0.6),-1.0)
+    val bot = new Perceptron(Signals(1.1,1.1),-1.0)
+    val out = new Perceptron(Signals(-2.0,1.1),-1.0)
+    val layers = List(Layer(top,bot),Layer(out))
+    val network = new Network(layers)
+    assert(0===network.backSweep(Signals(0.0,0.0)).head.head)
+    assert(1===network.backSweep(Signals(1.0,0.0)).head.head)
+    assert(1===network.backSweep(Signals(0.0,1.0)).head.head)
+    assert(0===network.backSweep(Signals(1.0,1.0)).head.head)
+  }
+
+  test("XOR Gate, Sigmoid Network") {
+    val top = new Sigmoid(Signals(1.0,1.0),0.0)
+    val bot = new Sigmoid(Signals(2.0,2.0),0.0)
+    val out = new Sigmoid(Signals(-1000.0,850.0),0.0)
+    val layers = List(Layer(top,bot),Layer(out))
+    val network = new Network(layers)
+    assert(0.25>network.backSweep(Signals(0.0,0.0)).head.head)
+    assert(0.75<network.backSweep(Signals(1.0,0.0)).head.head)
+    assert(0.75<network.backSweep(Signals(0.0,1.0)).head.head)
+    assert(0.25>network.backSweep(Signals(1.0,1.0)).head.head)
   }
 
 }
